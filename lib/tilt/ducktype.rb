d66c54cc98ec636719e0849540289be86823e156
Garrett LeSage <garrett@lesage.us>
2015-02-10 17:49:01 +0100

    actually do things! …and mostly as expected!

--- lib/tilt/ducktype.rb
+++ lib/tilt/ducktype.rb
@@ -5,12 +5,27 @@ module Tilt
   class DucktypeTemplate < Template
     def prepare
       #(data?)
+      @tmpdir = "/tmp/"
+      @tmpfile = "tilt-ducktype-file"
+      @input = data
       @output = nil
     end
 
     def evaluate(scope, locals, &block)
       # Do some sort of evaluation stuff with shelling out
-      @output ||= "FOO! #{scope}, #{locals}, #{block}"
+      #@output ||= "Foo000oooo #{scope.public_methods}, #{scope.instance_variables}, #{locals}, #{block}"
+      File.write "#{@tmpdir}#{@tmpfile}", @input
+
+      begin
+        `ducktype #{@tmpdir}#{@tmpfile} -o #{@tmpdir}#{@tmpfile}.page`
+        `cd #{@tmpdir} && yelp-build html #{@tmpdir}#{@tmpfile}.page`
+
+        doc = Nokogiri::HTML(File.read "#{@tmpdir}#{@tmpfile}.html")
+
+        @output ||= doc.css('body').to_s
+      rescue
+        @output = "Please install both ducktype and yelp-build."
+      end
     end
   end
 
